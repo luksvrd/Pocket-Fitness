f73720a9dcbb6576aa4e061e889ed834e568a915
Joshua <janorris65@gmail.com>
2022-11-29 18:32:05 -0600

    footer hide

--- src/index.js
+++ src/index.js
@@ -135,6 +135,7 @@ $("#buyNow").on("click", function (event) {
   exerciseCounter = 0;
   $("#reps").text("");
   $("main").show();
+  $("footer").hide();
 });
 
 // code block to reStart button
@@ -148,6 +149,7 @@ $("#reStart").on("click", function (event) {
   exerciseCounter = 0;
   $("#reps").text("");
   $("main").show();
+  $("footer").hide();
 });
 // function to call API
 function callmusclefunction(type, difficulty) {
